b4cc4a586830c68d4083f81070016df219b90310
Emil Mamedov <ea.mamedov@proton.me>
2022-08-28 20:48:52 +0300

    initial commit

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,375 @@
+-------------------------------------------------------------------------------
+-- These are example settings to use with nvim-metals and the nvim built-in
+-- LSP. Be sure to thoroughly read the `:help nvim-metals` docs to get an
+-- idea of what everything does. Again, these are meant to serve as an example,
+-- if you just copy pasta them, then should work,  but hopefully after time
+-- goes on you'll cater them to your own liking especially since some of the stuff
+-- in here is just an example, not what you probably want your setup to be.
+--
+-- Unfamiliar with Lua and Neovim?
+--  - Check out https://github.com/nanotee/nvim-lua-guide
+--
+-- The below configuration also makes use of the following plugins besides
+-- nvim-metals, and therefore is a bit opinionated:
+--
+-- - https://github.com/hrsh7th/nvim-cmp
+--   - hrsh7th/cmp-nvim-lsp for lsp completion sources
+--   - hrsh7th/cmp-vsnip for snippet sources
+--   - hrsh7th/vim-vsnip for snippet sources
+--
+-- - https://github.com/wbthomason/packer.nvim for package management
+-- - https://github.com/mfussenegger/nvim-dap (for debugging)
+-------------------------------------------------------------------------------
+local api = vim.api
+local cmd = vim.cmd
+local opt = vim.opt
+
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Basics
+opt.number = true
+opt.relativenumber = true
+opt.mouse = "a"
+opt.showmode = false
+opt.showmatch = true
+opt.tabstop = 2
+opt.softtabstop = 2 
+opt.smarttab = true
+opt.shiftwidth = 2
+
+----------------------------------
+-- PLUGINS -----------------------
+----------------------------------
+cmd([[packadd packer.nvim]])
+require("packer").startup(function(use)
+  use({ "wbthomason/packer.nvim", opt = true })
+
+  use 'rmehri01/onenord.nvim'
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icons
+    }
+  }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+	'romgrk/barbar.nvim',
+	requires = {'kyazdani42/nvim-web-devicons'}
+  }
+  use('glepnir/dashboard-nvim')
+  use('lewis6991/gitsigns.nvim')
+  use('lukas-reineke/indent-blankline.nvim')
+
+  use({
+    "hrsh7th/nvim-cmp",
+    requires = {
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-vsnip" },
+      { "hrsh7th/vim-vsnip" },
+    },
+  })
+  use({
+    "scalameta/nvim-metals",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "mfussenegger/nvim-dap",
+    },
+  })
+end)
+
+----------------------------------
+-- OPTIONS -----------------------
+----------------------------------
+-- global
+vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
+vim.opt_global.shortmess:remove("F"):append("c")
+
+-- custom mappings
+map("n", "<A-j>", "<cmd>m .+1<CR>") -- move line down
+map("n", "<A-k>", "<cmd>m .-2<CR>") -- move line up
+
+-- LSP mappings
+map("n", "gD", "<cmd>lua vim.lsp.buf.definition()<CR>")
+map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+map("n", "gds", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
+map("n", "gws", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
+map("n", "<leader>cl", [[<cmd>lua vim.lsp.codelens.run()<CR>]])
+map("n", "<leader>sh", [[<cmd>lua vim.lsp.buf.signature_help()<CR>]])
+map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+map("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+map("n", "<leader>ws", '<cmd>lua require"metals".hover_worksheet()<CR>')
+map("n", "<leader>aa", [[<cmd>lua vim.diagnostic.setqflist()<CR>]]) -- all workspace diagnostics
+map("n", "<leader>ae", [[<cmd>lua vim.diagnostic.setqflist({severity = "E"})<CR>]]) -- all workspace errors
+map("n", "<leader>aw", [[<cmd>lua vim.diagnostic.setqflist({severity = "W"})<CR>]]) -- all workspace warnings
+map("n", "<leader>d", "<cmd>lua vim.diagnostic.setloclist()<CR>") -- buffer diagnostics only
+map("n", "[c", "<cmd>lua vim.diagnostic.goto_prev { wrap = false }<CR>")
+map("n", "]c", "<cmd>lua vim.diagnostic.goto_next { wrap = false }<CR>")
+
+-- Example mappings for usage with nvim-dap. If you don't use that, you can
+-- skip these
+map("n", "<leader>dc", [[<cmd>lua require"dap".continue()<CR>]])
+map("n", "<leader>dr", [[<cmd>lua require"dap".repl.toggle()<CR>]])
+map("n", "<leader>dK", [[<cmd>lua require"dap.ui.widgets".hover()<CR>]])
+map("n", "<leader>dt", [[<cmd>lua require"dap".toggle_breakpoint()<CR>]])
+map("n", "<leader>dso", [[<cmd>lua require"dap".step_over()<CR>]])
+map("n", "<leader>dsi", [[<cmd>lua require"dap".step_into()<CR>]])
+map("n", "<leader>dl", [[<cmd>lua require"dap".run_last()<CR>]])
+
+-- Telescope mappings
+map("n", "<leader>ff", [[<cmd>lua require"telescope.builtin".find_files()<CR>]])
+map("n", "<leader>fg", [[<cmd>lua require"telescope.builtin".live_grep()<CR>]])
+map("n", "<leader>fb", [[<cmd>lua require"telescope.builtin".buffers()<CR>]])
+map("n", "<leader>fh", [[<cmd>lua require"telescope.builtin".help_tags()<CR>]])
+
+-- Barbar mappings
+-- Move to previous/next
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+-- Re-order to previous/next
+map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+-- Goto buffer in position...
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+-- Pin/unpin buffer
+map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+-- Close buffer
+map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+-- Magic buffer-picking mode
+map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+-- Sort automatically by...
+map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+
+-- NvimTree mappings
+map('n', '<F5>', '<Cmd>NvimTreeToggle<CR>')
+
+-- colorsheme setup
+local colors = require("onenord.colors").load()
+local local_onenord = require('lualine.themes.onenord')
+require("onenord").setup()
+
+-- nvim-tree setup
+require("nvim-tree").setup({
+  view = {
+    adaptive_size = true
+  }
+})
+
+-- lualine setup
+require("lualine").setup {
+	options = {
+		theme = local_onenord
+	}
+}
+
+-- nvim-treesitter setup
+require("nvim-treesitter.configs").setup({
+  ensure_insatlled = { 
+	"scala", 
+	"lua", 
+	"java", 
+	"bash", 
+	"hocon", 
+	"json", 
+	"javascript", 
+	"typescript", 
+	"html", 
+	"css", 
+	"scss", 
+	"python", 
+	"sql", 
+	"yaml", 
+	"proto", 
+	"markdown", 
+	"dockerfile"
+  },
+  sync_install = false,
+  ignore_install = {""},
+  highlight = {
+    enable = true,
+    disable = {""},
+    additional_vim_regex_highlighting = true
+  },
+  indent = { enable = true, disable = {""} }
+})
+
+-- telescope setup
+require("telescope").setup()
+
+-- barbar setup
+require("bufferline").setup({
+  animation = true,
+  tabpages = true,
+  clickable = true,
+  icons = true,
+  maximum_padding = 1,
+  maximum_length = 30,
+  icon_custom_colors = false
+})
+
+local nvim_tree_events = require('nvim-tree.events')
+local bufferline_state = require('bufferline.state')
+local nvim_tree_view = require('nvim-tree.view')
+
+local function get_tree_size()
+  return nvim_tree_view.View.width 
+end
+
+nvim_tree_events.subscribe('TreeOpen', function()
+  bufferline_state.set_offset(get_tree_size())
+end)
+
+nvim_tree_events.subscribe('Resize', function()
+  bufferline_state.set_offset(get_tree_size())
+end)
+
+nvim_tree_events.subscribe('TreeClose', function()
+  bufferline_state.set_offset(0)
+end)
+
+-- dashboard setup
+require("dashboard")
+
+-- gitsigns setup
+require("gitsigns").setup({
+  current_line_blame = true
+})
+
+-- indent-blankline setup
+opt.listchars:append "eol:↴"
+
+require("indent_blankline").setup {
+    show_current_context = true,
+    show_current_context_start = true,
+}
+
+-- completion related settings
+-- This is similiar to what I use
+local cmp = require("cmp")
+cmp.setup({
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+  },
+  snippet = {
+    expand = function(args)
+      -- Comes from vsnip
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- None of this made sense to me when first looking into this since there
+    -- is no vim docs, but you can't have select = true here _unless_ you are
+    -- also using the snippet stuff. So keep in mind that if you remove
+    -- snippets you need to remove this select
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    -- I use tabs... some say you should stick to ins-completion but this is just here as an example
+    ["<Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end,
+    ["<S-Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end,
+  }),
+})
+
+----------------------------------
+-- LSP Setup ---------------------
+----------------------------------
+local metals_config = require("metals").bare_config()
+
+-- Example of settings
+metals_config.settings = {
+  showImplicitArguments = true,
+  excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+}
+
+-- *READ THIS*
+-- I *highly* recommend setting statusBarProvider to true, however if you do,
+-- you *have* to have a setting to display this in your statusline or else
+-- you'll not see any messages from metals. There is more info in the help
+-- docs about this
+-- metals_config.init_options.statusBarProvider = "on"
+
+-- Example if you are using cmp how to make sure the correct capabilities for snippets are set
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+metals_config.capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+
+-- Debug settings if you're using nvim-dap
+local dap = require("dap")
+
+dap.configurations.scala = {
+  {
+    type = "scala",
+    request = "launch",
+    name = "RunOrTest",
+    metals = {
+      runType = "runOrTestFile",
+      --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+    },
+  },
+  {
+    type = "scala",
+    request = "launch",
+    name = "Test Target",
+    metals = {
+      runType = "testTarget",
+    },
+  },
+}
+
+metals_config.on_attach = function(client, bufnr)
+  require("metals").setup_dap()
+end
+
+-- Autocmd that will actually be in charging of starting the whole thing
+local nvim_metals_group = api.nvim_create_augroup("nvim-metals", { clear = true })
+api.nvim_create_autocmd("FileType", {
+  -- NOTE: You may or may not want java included here. You will need it if you
+  -- want basic Java support but it may also conflict if you are using
+  -- something like nvim-jdtls which also works on a java filetype autocmd.
+  pattern = { "scala", "sbt", "java" },
+  callback = function()
+    require("metals").initialize_or_attach(metals_config)
+  end,
+  group = nvim_metals_group,
+})
