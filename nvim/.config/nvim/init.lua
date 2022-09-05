7cd0a5b6c372c98cc669ef00488e9e843dbb5a64
Emil Mamedov <ea.mamedov@proton.me>
2022-09-05 22:40:04 +0300

    add lspsaga and toggleterm to nvim plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -84,6 +84,8 @@ require("packer").startup(function(use)
 	use('ellisonleao/glow.nvim')
 	use('kdheepak/lazygit.nvim')
 	use('numToStr/Comment.nvim')
+	use { "glepnir/lspsaga.nvim", branch = "main" }
+	use { "akinsho/toggleterm.nvim", tag = '*' }
 
   use({
     "hrsh7th/nvim-cmp",
@@ -185,6 +187,42 @@ map('n', '<F5>', '<Cmd>NvimTreeToggle<CR>')
 
 -- Lazygit mappings
 map('n', '<leader>gg', '<Cmd>LazyGit<CR>')
+
+-- Toggleterm mappings
+require("toggleterm").setup {
+	size = 20,
+	open_mapping = [[<c-\>]],
+	hide_numbers = true,
+	shade_filetypes = {},
+	shade_terminals = true,
+	shading_factor = 2,
+	start_in_insert = true,
+	insert_mappings = true,
+	persist_size = true,
+	direction = "float",
+	close_on_exit = true,
+	shell = vim.o.shell,
+	float_opts = {
+		border = "curved",
+		winblend = 0,
+		highlights = {
+			border = "Normal",
+			background = "Normal",
+		},
+	},
+}
+
+function _G.set_terminal_keymaps()
+  local opts = {noremap = true}
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 -- colorsheme setup
 local colors = require("onenord.colors").load()
@@ -314,6 +352,12 @@ require("glow").setup()
 
 -- comment setup
 require("Comment").setup()
+
+-- toggleterm setup
+require("toggleterm").setup()
+
+-- lspsaga setup
+require('lspsaga').init_lsp_saga()
 
 -- completion related settings
 -- This is similiar to what I use
