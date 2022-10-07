54384b504e21aac6ac45b2b9f81a71381c2fa9a7
Emil Mamedov <ea.mamedov@proton.me>
2022-10-07 15:58:16 +0300

    use new bufferline api

--- nvim/.config/nvim/lua/tachikoma/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/tachikoma/plugins/bufferline.lua
@@ -14,7 +14,7 @@ bufferline.setup {
 }
 
 local nvim_tree_events = require('nvim-tree.events')
-local bufferline_state = require('bufferline.state')
+local bufferline_api = require('bufferline.api')
 local nvim_tree_view = require('nvim-tree.view')
 
 local function get_tree_size()
@@ -22,13 +22,13 @@ local function get_tree_size()
 end
 
 nvim_tree_events.subscribe('TreeOpen', function()
-  bufferline_state.set_offset(get_tree_size())
+  bufferline_api.set_offset(get_tree_size())
 end)
 
 nvim_tree_events.subscribe('Resize', function()
-  bufferline_state.set_offset(get_tree_size())
+  bufferline_api.set_offset(get_tree_size())
 end)
 
 nvim_tree_events.subscribe('TreeClose', function()
-  bufferline_state.set_offset(0)
+  bufferline_api.set_offset(0)
 end)
