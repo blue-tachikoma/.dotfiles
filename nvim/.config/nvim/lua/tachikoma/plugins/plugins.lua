1cea4c463ae76d5cdf3cf8b84e9b52d963812bbe
Emil Mamedov <ea.mamedov@proton.me>
2022-10-31 23:27:50 +0300

    add autopairs plugin to nvim

--- nvim/.config/nvim/lua/tachikoma/plugins/plugins.lua
+++ nvim/.config/nvim/lua/tachikoma/plugins/plugins.lua
@@ -103,6 +103,7 @@ return packer.startup(function(use)
     "akinsho/toggleterm.nvim",
     tag = '*'
   }
+  use("windwp/nvim-autopairs")
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
