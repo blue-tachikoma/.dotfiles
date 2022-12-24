5a10b71d26d91fc053d167eb895df804dd6616d0
Emil Mamedov <ea.mamedov@proton.me>
2022-12-24 04:26:06 +0300

    nvim: support volar lsp for vue

--- nvim/.config/nvim/lua/tachikoma/plugins/lsp.lua
+++ nvim/.config/nvim/lua/tachikoma/plugins/lsp.lua
@@ -13,8 +13,47 @@ if not lspconfig_status_ok then
   return
 end
 
+local lspconfig_util_status_ok, lspconfig_util = pcall(require, "lspconfig.util")
+if not lspconfig_util_status_ok then
+  return
+end
+
+-- local lspconfig_configs_status_ok, lspconfig_configs = pcall(require, "lspconfig.configs")
+-- if not lspconfig_configs_status_ok then
+--   return
+-- end
+
 mason.setup()
 mason_lspconfig.setup {
-  ensure_installed = { "sumneko_lua" }
+  ensure_installed = { "sumneko_lua", "volar" }
 }
+
+-- Lua
 lspconfig.sumneko_lua.setup {}
+
+-- Vue
+lspconfig.volar.setup {
+  root_dir = lspconfig_util.root_pattern 'package.json',
+  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
+  init_options = {
+    languageFeatures = {
+      implementation = true,
+      references = true,
+      definition = true,
+      typeDefinition = true,
+      callHierarchy = true,
+      hover = true,
+      rename = true,
+      renameFileRefactoring = true,
+      signatureHelp = true,
+      codeAction = true,
+      workspaceSymbol = true,
+      completion = {
+        defaultTagNameCase = 'both',
+        defaultAttrNameCase = 'kebabCase',
+        getDocumentNameCasesRequest = false,
+        getDocumentSelectionRequest = false,
+      }
+    }
+  }
+}
