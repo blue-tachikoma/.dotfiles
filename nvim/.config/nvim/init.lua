0b12c08df06d0890cc020823cd246dfb242eecf5
Emil Mamedov <ea.mamedov@proton.me>
2022-12-27 02:50:11 +0300

    nvim: refactor plugins setup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,4 @@
 require "tachikoma.options"
 require "tachikoma.keymaps"
-require "tachikoma.plugins.plugins"
-require "tachikoma.plugins_setup"
+require "tachikoma.plugins"
+require "tachikoma.packer"
