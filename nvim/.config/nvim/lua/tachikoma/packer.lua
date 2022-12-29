5882b0162b09f5b8cc7e5b6e4817e3e73090de46
Emil Mamedov <ea.mamedov@proton.me>
2022-12-29 20:04:11 +0300

    nvim: add notify plugin

--- nvim/.config/nvim/lua/tachikoma/packer.lua
+++ nvim/.config/nvim/lua/tachikoma/packer.lua
@@ -97,6 +97,7 @@ return packer.startup(function(use)
 	use { "akinsho/toggleterm.nvim", tag = "*" }
 	use { "rmagatti/auto-session" }
 	use { "ellisonleao/glow.nvim" }
+  use { "rcarriga/nvim-notify" }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
