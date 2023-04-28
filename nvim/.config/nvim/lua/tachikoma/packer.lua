5ccb0b91613d38bc9d760ed78274ab0e26e936c1
Emil Mamedov <ea.mamedov@proton.me>
2023-04-28 17:58:57 +0300

    nvim: use latest telescope version

--- nvim/.config/nvim/lua/tachikoma/packer.lua
+++ nvim/.config/nvim/lua/tachikoma/packer.lua
@@ -94,7 +94,7 @@ return packer.startup(function(use)
   use { "windwp/nvim-autopairs" }
 
   -- Misc
-  use { "nvim-telescope/telescope.nvim", tag = "0.1.0" }
+  use { "nvim-telescope/telescope.nvim", tag = "0.1.1" }
   use { "akinsho/toggleterm.nvim", tag = "*" }
   use { "rmagatti/auto-session" }
   use { "ellisonleao/glow.nvim" }
