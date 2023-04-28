d809c8193e095ac6cd714cadc538a456dfa20615
Emil Mamedov <ea.mamedov@proton.me>
2023-04-28 18:04:16 +0300

    nvim: remove session name from lualine

--- nvim/.config/nvim/lua/tachikoma/plugins/lualine.lua
+++ nvim/.config/nvim/lua/tachikoma/plugins/lualine.lua
@@ -7,6 +7,5 @@ local local_onenord = require('lualine.themes.onenord')
 lualine.setup {
 	options = {
 		theme = local_onenord,
-	},
-	sections = {lualine_c = {require('auto-session-library').current_session_name}}
+	}
 }
