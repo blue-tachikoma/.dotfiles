b009521aa931c5ffc3236c0d244e1f7e8c898f33
Emil Mamedov <ea.mamedov@proton.me>
2023-04-28 17:58:06 +0300

    nvim: remove volar lsp config

--- nvim/.config/nvim/lua/tachikoma/plugins/lsp.lua
+++ nvim/.config/nvim/lua/tachikoma/plugins/lsp.lua
@@ -13,42 +13,10 @@ if not lspconfig_status_ok then
   return
 end
 
-local lspconfig_util_status_ok, lspconfig_util = pcall(require, "lspconfig.util")
-if not lspconfig_util_status_ok then
-  return
-end
-
 mason.setup()
 mason_lspconfig.setup {
-  ensure_installed = { "lua_ls", "volar" }
+  ensure_installed = { "lua_ls" }
 }
 
 -- Lua
 lspconfig.lua_ls.setup {}
-
--- Vue
-lspconfig.volar.setup {
-  root_dir = lspconfig_util.root_pattern 'package.json',
-  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
-  init_options = {
-    languageFeatures = {
-      implementation = true,
-      references = true,
-      definition = true,
-      typeDefinition = true,
-      callHierarchy = true,
-      hover = true,
-      rename = true,
-      renameFileRefactoring = true,
-      signatureHelp = true,
-      codeAction = true,
-      workspaceSymbol = true,
-      completion = {
-        defaultTagNameCase = 'both',
-        defaultAttrNameCase = 'kebabCase',
-        getDocumentNameCasesRequest = false,
-        getDocumentSelectionRequest = false,
-      }
-    }
-  }
-}
